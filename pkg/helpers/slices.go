833674592ea9f05941d388c0f874e12c5f7ce4c8
Ignacio Olave <me@iolave.com>
2025-04-10 11:32:48 -0400

    added method to retrieve lxc info by id

---
+++ pkg/helpers/slices.go
@@ -0,0 +1,11 @@
+package helpers
+
+func FilterSlice[T any](s []T, f func(T) bool) []T {
+	var result []T
+	for _, v := range s {
+		if f(v) {
+			result = append(result, v)
+		}
+	}
+	return result
+}
