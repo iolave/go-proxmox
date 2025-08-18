e796e0b80ec0bdcf12e5601a60ea16021dbf16ec
Ignacio Olave <me@iolave.com>
2025-08-18 19:39:40 -0400

    feat: added /api2/json/cluster/log implementations

---
+++ pkg/api/cluster_log.go
@@ -0,0 +1,30 @@
+package api
+
+import "net/http"
+
+type ClusterGetLogRequest struct {
+	// Maximum number of entries (1 - N)
+	MaxEntries int `in:"query=max;omitempty"`
+}
+
+// ClusterGetLog Read cluster log.
+func (c API) ClusterGetLog(req ClusterGetLogRequest) (res []struct {
+	ID      string `json:"id"`
+	Message string `json:"msg"`
+	UID     string `json:"uid"`
+	Time    int    `json:"time"`
+	Node    string `json:"node"`
+	PID     int    `json:"pid"`
+	Pri     int    `json:"pri"`
+	User    string `json:"user"`
+	Tag     string `json:"tag"`
+}, err error) {
+	err = c.SendPVERequest(PVERequest{
+		Path:    "/api2/json/cluster/log",
+		Method:  http.MethodGet,
+		Payload: &req,
+		Result:  &res,
+	})
+
+	return res, err
+}
